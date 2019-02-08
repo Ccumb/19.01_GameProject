de0e0bcd1961e40411ce9f2c58c332336742b949
JoTaeYang <whxodid666@gmail.com>
2019-02-08 22:22:29 +0900

    인벤토리 : 퀵 슬롯에 등록하는것을 제외한 기능 대부분 구현. 퀵 슬롯 등록은 마우스로 이동하는 걸로 할 예정

---
+++ 19.01GameProject/Assets/Scripts/UI/ObservableStack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void UpdateStackEvent();
+
+public class ObservableStack<T> : Stack<T>
+{
+    public event UpdateStackEvent OnPush;
+    public event UpdateStackEvent OnPop;
+    public event UpdateStackEvent OnClear;
+
+    public new void Push(T item)
+    {
+        base.Push(item);
+
+        if(OnPush != null)
+        {
+            OnPush();
+        }
+    }
+
+    public new T Pop()
+    {
+        T item = base.Pop();
+
+        if(OnPop != null)
+        {
+            OnPop();
+        }
+
+        return item;
+    }
+
+    public new void Clear()
+    {
+        base.Clear();
+
+        if(OnClear != null)
+        {
+            OnClear();
+        }
+    }
+
+}
