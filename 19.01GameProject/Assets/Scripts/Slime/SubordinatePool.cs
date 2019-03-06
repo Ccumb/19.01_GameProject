d89bfdc43b160e0d400d85ea4ceeee4606bd160c
Seojihoon <sjh11un@naver.com>
2019-03-06 20:21:36 +0900

    1.애니메이션 적용 2.패턴3 쫄 소환 기능 얼추 구현

---
+++ 19.01GameProject/Assets/Scripts/Slime/SubordinatePool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubordinatePool : MonoBehaviour
+{
+    public int ObjectCount;
+    public GameObject SubordinateObject;
+    private List<GameObject> PoolList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectPool();
+    }
+
+    public void ObjectPool()
+    {
+        for (int i = 0; i < ObjectCount; i++)
+        {
+            GameObject subordinate = Instantiate(SubordinateObject);
+            subordinate.transform.SetParent(transform.parent.GetChild(1));
+            //subordinate.layer = 11;
+            subordinate.SetActive(false);
+            PoolList.Add(subordinate);
+        }
+    }
+
+    public void Pooling(int poolCount, Transform[] poolPosition)
+    {
+        for (int i = 0; i < poolCount; i++)
+        {
+            if (PoolList[i].activeSelf == false)
+            {
+                PoolList[i].SetActive(true);
+                PoolList[i].transform.position = poolPosition[i].position;
+            }
+        }
+    }
+}
