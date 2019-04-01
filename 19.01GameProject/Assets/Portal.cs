18265afeeb044e2b29c64417f725c9e880337fb7
NeRemNem <hhc940419@gmail.com>
2019-04-01 20:47:52 +0900

    포탈 구현 // 충돌시 해당스테이지 이동

---
+++ 19.01GameProject/Assets/Portal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(BoxCollider))]
+public class Portal : MonoBehaviour
+{
+    private BoxCollider mCollider;
+    public string destination;
+    void Start()
+    {
+        mCollider = GetComponent<BoxCollider>();
+        mCollider.isTrigger = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(destination);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(destination);
+        }
+    }
+}
---
+++ 19.01GameProject/Assets/_HamChan/03.Scripts/Editor/PortalEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+[CustomEditor(typeof(Portal))]
+public class PortalEditor : Editor
+{
+    public string[] stages = new string[] { "PortalTest A", "PortalTest B", "PortalTest C", "PortalTest D" };
+    public int index;
+   
+    public override void OnInspectorGUI()
+    {
+        Portal portal = (Portal)target;
+        index = EditorGUILayout.Popup(index, stages);
+        if (GUILayout.Button("Set Destination"))
+        {
+            portal.destination = stages[index];
+            EditorUtility.SetDirty(target);
+            EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+        }
+        base.OnInspectorGUI();
+    }
+    
+}
