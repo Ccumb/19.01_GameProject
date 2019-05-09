c6357de48a38d9ba7609c3508bc81684f179b727
Seojihoon <sjh11un@naver.com>
2019-05-09 20:44:06 +0900

    보스 처치 시 아이템 뜨도록 구현 완료 //다음에 구현해야 할 것// 아이템을 먹었을 때 인벤토리에 등록되는 스크립트 제작, 이 스크립트에 아이템에 대한 정보가 들어갈 수 있도록 해야함

--- 19.01GameProject/Assets/Scripts/ItemObjectPooling.cs
+++ 19.01GameProject/Assets/Scripts/ItemObjectPooling.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class ItemObjectPooling : MonoBehaviour
 {
     public string[] ItemNames;  //게임 아이템 이름
-    public List<ItemIconVersion> ItemObjects    //아이템 정보 사용 리스트(공용)
+    public List<GameObject> ItemObjects    //아이템 정보 사용 리스트(공용)
     {
         get { return mObjects; }
     }
-    private List<ItemIconVersion> mObjects = new List<ItemIconVersion>(); //아이템 정보를 넣을 리스트(개인)
+    private List<GameObject> mObjects = new List<GameObject>(); //아이템 정보를 넣을 리스트(개인)
     private int mItemCount; //사용할 아이템 개수
 
     private void Start()
@@ -24,7 +24,12 @@ public class ItemObjectPooling : MonoBehaviour
                 if(item.name == ItemNames[j])
                 {
                     //이곳에서 아이템 미리 생성, 정보를 넣음
-                    mObjects.Add(item);
+                    Debug.Log(item.MyItem);
+                    GameObject dropItem = Instantiate(item.MyItem); //아이템에 있는 게임 오브젝트
+                    //dropItem.MyItemInfo = item;
+                    dropItem.transform.SetParent(gameObject.transform);
+                    dropItem.SetActive(false);
+                    mObjects.Add(dropItem);
                 }
             }
         }
