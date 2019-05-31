cd55fbf577eafe47828bd590591d1a67722a81e1
LeeHyuna <hyuna836539@gmail.com>
2019-05-31 19:43:55 +0900

    근거리 공격 이펙트 적용

---
+++ 19.01GameProject/Assets/Scripts/PlayerEffect.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEffect : MonoBehaviour
+{
+    public GameObject footStep;
+    public GameObject attackSkill;
+
+    public Transform footPos;
+    public Transform punchPos;
+
+    private List<GameObject> mFootSteps;
+    private List<GameObject> mAttacks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mFootSteps = new List<GameObject>();
+        mAttacks = new List<GameObject>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject foot = Instantiate(footStep, footPos);
+            foot.SetActive(false);
+            mFootSteps.Add(foot);
+        }
+
+        for(int i = 0; i < 3; i++)
+        {
+            GameObject punch = Instantiate(attackSkill, punchPos);
+            punch.SetActive(false);
+            mAttacks.Add(punch);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FootStep()
+    {
+        for(int i = 0; i < mFootSteps.Count; i++)
+        {
+            if(mFootSteps[i].active == false)
+            {
+                mFootSteps[i].active = true;
+                break;
+            }
+        }
+        StartCoroutine("FootDelay");
+    }
+
+    IEnumerator FootDelay()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        for (int i = 0; i < mFootSteps.Count; i++)
+        {
+            if (mFootSteps[i].active == true)
+            {
+                mFootSteps[i].active = false;
+                break;
+            }
+        }
+    }
+
+    void SkillAttack()
+    {
+        for (int i = 0; i < mAttacks.Count; i++)
+        {
+            if (mAttacks[i].active == false)
+            {
+                mAttacks[i].active = true;
+                break;
+            }
+        }
+        StartCoroutine("AttackDelay");
+    }
+
+    IEnumerator AttackDelay()
+    {
+        yield return new WaitForSeconds(1.8f);
+
+        for (int i = 0; i < mFootSteps.Count; i++)
+        {
+            if (mAttacks[i].active == true)
+            {
+                mAttacks[i].active = false;
+                break;
+            }
+        }
+    }
+}
