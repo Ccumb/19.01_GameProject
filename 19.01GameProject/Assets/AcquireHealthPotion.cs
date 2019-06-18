06a88b44e1fad9fb0a9c33e93b2197dc17a69843
JoTaeYang <whxodid666@gmail.com>
2019-06-18 17:39:45 +0900

    아이템 줏어먹기 추가

---
+++ 19.01GameProject/Assets/AcquireHealthPotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcquireHealthPotion : MonoBehaviour
+{
+    public ItemIconVersion itemData;
+    // Start is called before the first frame update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            InventoryScript.MyInstance.AddItem(itemData);
+            Destroy(this.gameObject);
+        }
+    }
+}
